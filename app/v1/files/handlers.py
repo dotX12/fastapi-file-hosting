699ea738ac56affd4fd49c47de79308322a91857
dotX12 <dev@shitposting.team>
2022-02-04 07:28:42 +0300

    list file info

--- app/v1/files/handlers.py
+++ app/v1/files/handlers.py
@@ -1,3 +1,4 @@
+from typing import List
 from uuid import UUID
 
 from fastapi import File, UploadFile, APIRouter, Form, Depends
@@ -6,8 +7,7 @@ from starlette.responses import JSONResponse, FileResponse
 
 from app.v1.files.dependencies import FileDependencyMarker
 from app.v1.files.crud import FileRepository
-from app.v1.files.misc import UPLOAD_FILE_TTL_DESCRIPTION
-from app.v1.files.schemas import responses
+from app.v1.files.schemas import GetFileModel, PostFormModel
 from app.v1.files.services import FileService
 
 files_router = APIRouter()
@@ -15,23 +15,22 @@ files_router = APIRouter()
 
 @files_router.post("/files")
 async def create_upload_file(
-    file: UploadFile = File(..., description="File to upload."),
-    ttl: int = Form(default=1, ge=1, le=180, description=UPLOAD_FILE_TTL_DESCRIPTION),
-    db: FileRepository = Depends(FileDependencyMarker),
-    file_service: FileService = Depends(),
+        data: PostFormModel = Depends(),
+        db: FileRepository = Depends(FileDependencyMarker),
+        file_service: FileService = Depends(),
 ):
-    file_expire_time = file_service.generate_delete_file_date(hours=ttl)
-    upload_file = await db.create_upload_file(upload_file=file, expire_time=file_expire_time)
+    file_expire_time = file_service.generate_delete_file_date(hours=data.ttl)
+    upload_file = await db.create_upload_file(upload_file=data.file, expire_time=file_expire_time)
 
     data = {"detail": "File created", "uuid": str(upload_file.uuid)}
     return JSONResponse(status_code=201, content=data)
 
 
-@files_router.get("/files/{file_uuid}/info", responses=responses)
+@files_router.get("/files/{file_uuid}/info", response_model=GetFileModel)
 async def get_file_info(
-    file_uuid: UUID = Field(alias="uuid"),
-    db: FileRepository = Depends(FileDependencyMarker),
-    file_service: FileService = Depends(),
+        file_uuid: UUID = Field(alias="uuid"),
+        db: FileRepository = Depends(FileDependencyMarker),
+        file_service: FileService = Depends(),
 ):
     result = await db.get_upload_file(uuid=file_uuid)
     return file_service.generate_response_model(
@@ -45,11 +44,30 @@ async def get_file_info(
     )
 
 
+@files_router.get("/files", response_model=List[GetFileModel])
+async def get_files_info(
+        db: FileRepository = Depends(FileDependencyMarker),
+        file_service: FileService = Depends(),
+):
+    result = await db.get_upload_files()
+    return [
+        file_service.generate_response_model(
+            uuid=element.uuid,
+            original_name=element.original_name,
+            size_bytes=element.size_bytes,
+            deleted_at=element.deleted_at,
+            status=element.status,
+            created_at=element.created_at,
+            mime_type=element.mime_type,
+        ) for element in result
+    ]
+
+
 @files_router.get("/files/{file_uuid}")
 async def get_file(
-    file_uuid: UUID = Field(alias="uuid"),
-    db: FileRepository = Depends(FileDependencyMarker),
-    file_service: FileService = Depends(),
+        file_uuid: UUID = Field(alias="uuid"),
+        db: FileRepository = Depends(FileDependencyMarker),
+        file_service: FileService = Depends(),
 ):
     file_db = await db.get_upload_file(uuid=file_uuid)
     file_path = file_service.file_path_from_security_name(file_db.security_name)
--- app/v1/files/schemas.py
+++ app/v1/files/schemas.py
@@ -2,11 +2,23 @@ from datetime import datetime
 from typing import Type, Any, Dict, Optional
 from uuid import UUID
 
+from fastapi import UploadFile, File, Form
 from pydantic import validator
 
 from app.schemas.base import BaseModelORM
+from app.v1.files.misc import UPLOAD_FILE_TTL_DESCRIPTION
 from app.v1.files.utils import convert_size
 from config import settings_app
+
+
+class PostFormModel:
+    def __init__(
+        self,
+        file: UploadFile = File(..., description="File to upload."),
+        ttl: int = Form(default=1, ge=1, le=180, description=UPLOAD_FILE_TTL_DESCRIPTION)
+    ):
+        self.file = file
+        self.ttl = ttl
 
 
 class URLModel(BaseModelORM):
@@ -14,7 +26,11 @@ class URLModel(BaseModelORM):
 
     @validator("minimal", always=True)
     def validate_minimal(cls: Type['URLModel'], v: Any) -> str:
-        return f"{settings_app.HOSTING_URL}/files/{v}"
+        try:
+            assert UUID(v)
+            return f"{settings_app.HOSTING_URL}/files/{v}"
+        except ValueError:
+            return v
 
 
 class SizeModel(BaseModelORM):
@@ -41,11 +57,3 @@ class GetFileModel(BaseModelORM):
     deleted_at: datetime
     available: bool
 
-
-responses = {
-    200: {
-        "model": GetFileModel,
-        "description": "Good request"
-    }
-
-}
