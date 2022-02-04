b2c9104f265ee8d6655f623fba5139de1f2e759c
dotX12 <dev@shitposting.team>
2022-02-04 07:05:53 +0300

    application

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.trustedhost import TrustedHostMiddleware
 
+from app.handlers.exceptions import sql_exception_handler
+from app.handlers.models import ExceptionSQL
 from app.v1.binding import own_router_v1
-
-app = FastAPI()
+from app.v1.files import FileRepository, FileDependencyMarker
+from misc import async_session
 
 
 def get_application_v1() -> FastAPI:
     application = FastAPI(
         debug=True,
-        docs_url=None,
         title='Files Hosting',
         version='1.2.15',
     )
@@ -28,5 +29,15 @@ def get_application_v1() -> FastAPI:
         allowed_hosts=['*']
     )
     application.include_router(own_router_v1)
+    application.add_exception_handler(ExceptionSQL, sql_exception_handler)
+
+    application.dependency_overrides.update(
+        {
+            FileDependencyMarker: lambda: FileRepository(db_session=async_session),
+        }
+    )
 
     return application
+
+
+app = get_application_v1()
